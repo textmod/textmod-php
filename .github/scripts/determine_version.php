1ac0064899e9f79d803ea1c2aefa087d5dde5d8f
Boris Claus Verhaaff <boris@c4rex.dev>
2023-05-22 09:57:54 +0200

    updated

--- .github/scripts/determine_version.php
+++ .github/scripts/determine_version.php
@@ -5,14 +5,29 @@ rsort($tags, SORT_NATURAL);
 $nextVersion = "";
 
 $inputVersion = $argv[1];
+$branch = $argv[2];
+
+// Validate the branch input
+if ($branch !== "php7" && $branch !== "php8") {
+    echo "Invalid branch. Please provide either 'php7' or 'php8' as the branch.";
+    return;
+}
 
 if (empty($tags)) {
-    // No existing tags, set a default starting version
-    $nextVersion = "1.0.0";
+    // No existing tags, set a default starting version based on the branch
+    switch ($branch) {
+        case "php7":
+            $nextVersion = "7.0.0";
+            break;
+        case "php8":
+        default:
+            $nextVersion = "8.0.0";
+            break;
+    }
 } else {
-    // Find the highest version
+    // Find the highest version without the branch suffix
     foreach ($tags as $tag) {
-        if (preg_match('/(\d+\.\d+\.\d+)/', $tag, $matches)) {
+        if (preg_match('/^(\d+\.\d+\.\d+)$/', $tag, $matches)) {
             $currentVersion = $matches[1];
             $nextVersion = $currentVersion;
             break;
@@ -20,8 +35,16 @@ if (empty($tags)) {
     }
 
     if (empty($nextVersion)) {
-        // No existing tag found, set a default starting version
-        $nextVersion = "1.0.0";
+        // No existing tag found without the branch suffix, set a default starting version based on the branch
+        switch ($branch) {
+            case "php7":
+                $nextVersion = "7.0.0";
+                break;
+            case "php8":
+            default:
+                $nextVersion = "8.0.0";
+                break;
+        }
     }
 }
 
@@ -29,17 +52,59 @@ switch ($inputVersion) {
     case "patch":
         $versionParts = explode(".", $nextVersion);
         $patchVersion = isset($versionParts[2]) ? (int)$versionParts[2] : 0;
-        $nextVersion = $versionParts[0] . "." . $versionParts[1] . "." . ($patchVersion + 1);
+
+        // Increment the patch version appropriately based on the branch
+        switch ($branch) {
+            case "php7":
+                if ($versionParts[0] == 7 && $versionParts[1] == 0 && $versionParts[2] == 0) {
+                    $nextVersion = $versionParts[0] . "." . $versionParts[1] . "." . ($patchVersion + 1);
+                } else {
+                    $nextVersion = $versionParts[0] . "." . $versionParts[1] . "." . $versionParts[2];
+                }
+                break;
+            case "php8":
+            default:
+                $nextVersion = $versionParts[0] . "." . $versionParts[1] . "." . ($patchVersion + 1);
+                break;
+        }
         break;
     case "minor":
         $versionParts = explode(".", $nextVersion);
         $minorVersion = isset($versionParts[1]) ? (int)$versionParts[1] : 0;
-        $nextVersion = $versionParts[0] . "." . ($minorVersion + 1) . ".0";
+
+        // Increment the minor version appropriately based on the branch
+        switch ($branch) {
+            case "php7":
+                if ($versionParts[0] == 7 && $versionParts[1] == 0 && $versionParts[2] == 0) {
+                    $nextVersion = $versionParts[0] . "." . ($minorVersion + 1) . ".0";
+                } else {
+                    $nextVersion = $versionParts[0] . "." . $versionParts[1] . ".0";
+                }
+                break;
+            case "php8":
+            default:
+                $nextVersion = $versionParts[0] . "." . ($minorVersion + 1) . ".0";
+                break;
+        }
         break;
     case "major":
         $versionParts = explode(".", $nextVersion);
         $majorVersion = isset($versionParts[0]) ? (int)$versionParts[0] : 0;
-        $nextVersion = ($majorVersion + 1) . ".0.0";
+
+        // Increment the major version appropriately based on the branch
+        switch ($branch) {
+            case "php7":
+                if ($versionParts[0] == 7 && $versionParts[1] == 0 && $versionParts[2] == 0) {
+                    $nextVersion = ($majorVersion + 1) . ".0.0";
+                } else {
+                    $nextVersion = $versionParts[0] . ".0.0";
+                }
+                break;
+            case "php8":
+            default:
+                $nextVersion = ($majorVersion + 1) . ".0.0";
+                break;
+        }
         break;
 }
 
