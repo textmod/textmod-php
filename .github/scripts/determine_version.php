dd03183d718be95e2d0b8485748c16a1c4ed8154
Boris Claus Verhaaff <boris@c4rex.dev>
2023-05-22 09:16:45 +0200

    updated

---
+++ .github/scripts/determine_version.php
@@ -0,0 +1,46 @@
+<?php
+$tags = explode("\n", shell_exec("git tag"));
+rsort($tags, SORT_NATURAL);
+
+$nextVersion = "";
+
+$inputVersion = $argv[1];
+
+if (empty($tags)) {
+    // No existing tags, set a default starting version
+    $nextVersion = "1.0.0";
+} else {
+    // Find the highest version
+    foreach ($tags as $tag) {
+        if (preg_match('/(\d+\.\d+\.\d+)/', $tag, $matches)) {
+            $currentVersion = $matches[1];
+            $nextVersion = $currentVersion;
+            break;
+        }
+    }
+
+    if (empty($nextVersion)) {
+        // No existing tag found, set a default starting version
+        $nextVersion = "1.0.0";
+    }
+}
+
+switch ($inputVersion) {
+    case "patch":
+        $versionParts = explode(".", $nextVersion);
+        $patchVersion = isset($versionParts[2]) ? (int)$versionParts[2] : 0;
+        $nextVersion = $versionParts[0] . "." . $versionParts[1] . "." . ($patchVersion + 1);
+        break;
+    case "minor":
+        $versionParts = explode(".", $nextVersion);
+        $minorVersion = isset($versionParts[1]) ? (int)$versionParts[1] : 0;
+        $nextVersion = $versionParts[0] . "." . ($minorVersion + 1) . ".0";
+        break;
+    case "major":
+        $versionParts = explode(".", $nextVersion);
+        $majorVersion = isset($versionParts[0]) ? (int)$versionParts[0] : 0;
+        $nextVersion = ($majorVersion + 1) . ".0.0";
+        break;
+}
+
+return $nextVersion;
